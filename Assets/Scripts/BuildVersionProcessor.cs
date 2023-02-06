a222f3f983bfd8c9b49c5bea5642e5a83f99eaab
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-02-06 14:27:59 +0100

    fixes

--- Assets/Scripts/BuildVersionProcessor.cs
+++ Assets/Scripts/BuildVersionProcessor.cs
@@ -1,33 +1,33 @@
-using System;
-using UnityEditor;
-using UnityEditor.Build;
-using UnityEditor.Build.Reporting;
-using UnityEngine;
+// using System;
+// using UnityEditor;
+// using UnityEditor.Build;
+// using UnityEditor.Build.Reporting;
+// using UnityEngine;
 
-public class BuildVersionProcessor : IPreprocessBuildWithReport
+public class BuildVersionProcessor //IPreprocessBuildWithReport
 {
-    public int callbackOrder => 0;
-    private const string initialVersion = "0.0";
+    // public int callbackOrder => 0;
+    // private const string initialVersion = "0.0";
     
-    public void OnPreprocessBuild(BuildReport report) {
-        string currentVersion = FindCurrentVersion();
-        UpdateVersion(currentVersion);
-    }
+    // public void OnPreprocessBuild(BuildReport report) {
+    //     // string currentVersion = FindCurrentVersion();
+    //     // UpdateVersion(currentVersion);
+    // }
 
-    private void UpdateVersion(string currentVersion)
-    {
-        if(float.TryParse(currentVersion, out float versionNumber)) {
-            float nextVersion = versionNumber + 0.01f;
-            string date = DateTime.Now.ToString("d");
+    // private void UpdateVersion(string currentVersion)
+    // {
+    //     if(float.TryParse(currentVersion, out float versionNumber)) {
+    //         float nextVersion = versionNumber + 0.01f;
+    //         string date = DateTime.Now.ToString("d");
 
-            PlayerSettings.bundleVersion = string.Format("Version [{0}] - {1}", nextVersion, date);
-        }
-    }
+    //         PlayerSettings.bundleVersion = string.Format("Version [{0}] - {1}", nextVersion, date);
+    //     }
+    // }
 
-    private string FindCurrentVersion()
-    {
-        string[] currentVersion = PlayerSettings.bundleVersion.Split('[', ']');
+    // private string FindCurrentVersion()
+    // {
+    //     string[] currentVersion = PlayerSettings.bundleVersion.Split('[', ']');
 
-        return currentVersion.Length == 1 ? initialVersion : currentVersion[1];
-    }
+    //     return currentVersion.Length == 1 ? initialVersion : currentVersion[1];
+    // }
 }
