565d8dea1b1e09b9ea2e9ab9aab2e73a74d8f2ff
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-01 17:05:38 +0200

    Add SO for projectiles

---
+++ Assets/ScriptableObjects/Values/ProjectileData.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Projectile Data", menuName = "ScriptableObjects/Values/ProjectileData", order = 0)]
+public class ProjectileData : ScriptableObject
+{
+    public float speed = 2;
+    public float torque = 0.1f;
+    public int damage = 1;
+    public Vector2 knockback = Vector2.zero;
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -25,19 +25,6 @@ public class Projectile : MonoBehaviour
         rb.AddForce(Vector2.left * projectileData.speed, ForceMode2D.Impulse);
     }
 
-    // private void FixedUpdate()
-    // {
-    //     RaycastHit2D[] listHits = Physics2D.RaycastAll(new Vector2(bc2d.bounds.min.x, bc2d.bounds.center.y), transform.right * Mathf.Sign(rb.velocity.x), 60, collisionLayers);
-    //     foreach (var hitInfo in listHits)
-    //     {
-    //         if (hitInfo)
-    //         {
-    //             print(hitInfo.transform.name);
-    //         }
-    //     }
-
-    // }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         rb.constraints = 0;
@@ -47,19 +34,9 @@ public class Projectile : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             IDamageable iDamageable = other.transform.GetComponentInChildren<IDamageable>();
-            // other.gameObject.TryGetComponent(out IDamageable iDamageable)
-            print("ffeee");
             iDamageable.TakeDamage(projectileData.damage);
         }
     }
-
-    // private void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     if (other.TryGetComponent(out IDamageable iDamageable) && other.CompareTag("Player"))
-    //     {
-    //         iDamageable.TakeDamage(2);
-    //     }
-    // }
 
     private void OnDrawGizmos()
     {
