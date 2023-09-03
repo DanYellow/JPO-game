c7690306b6a1a91705dcff433abcef9ddf461f32
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-03 15:24:38 +0200

    Add light to candelabrum

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Checkpoint : MonoBehaviour
 {
     private BoxCollider2D bc2d;
     private Animator animator;
 
-    private void Awake() {
+    private new Light2D light;
+
+    private void Awake()
+    {
         bc2d = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
+
+        light = GetComponentInChildren<Light2D>(true);
+        light.enabled = false;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,6 +23,7 @@ public class Checkpoint : MonoBehaviour
             animator.SetTrigger(AnimationStrings.activate);
             playerSpawn.currentSpawnPosition = transform.position;
             bc2d.enabled = false;
+            light.enabled = true;
         }
     }
 }
