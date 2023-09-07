b974dc0e51861e1ad9017721b3b52db9040abd34
PC-Dan <jeanlouis.danielo@sfr.fr>
2023-09-07 18:55:23 +0200

    Fix Followed component

--- Assets/Scripts/Followed.cs
+++ Assets/Scripts/Followed.cs
@@ -4,16 +4,19 @@ public class Followed : MonoBehaviour
 {
     [SerializeField] Transform follower;
     private Collider2D bc;
+
+    private float yDelta = 0;
     private void Awake()
     {
         bc = GetComponent<Collider2D>();
+        yDelta = Mathf.Abs(bc.bounds.center.y - follower.position.y);
     }
 
     void Update()
     {
         follower.position = new Vector3(
             bc.bounds.center.x,
-            follower.position.y,
+            yDelta + bc.bounds.center.y,
             follower.position.z
         );
     }
