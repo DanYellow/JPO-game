43696f686cec5d253fadfa706940ad45dd46e629
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-01-07 17:41:59 +0100

    Add setup script for mechaboss

--- Assets/Scripts/MechaBossSpike.cs
+++ Assets/Scripts/MechaBossSpike.cs
@@ -6,15 +6,46 @@ using UnityEngine;
 
 public class MechaBossSpike : MonoBehaviour
 {
+    [SerializeField]
+    ProjectileData projectileData;
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    Rigidbody2D rb;
+
+    bool fire = false;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fire = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (fire)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.velocity = (GameObject.Find("Player").transform.position - transform.position).normalized * 15; // speed
+            fire= false;
+        }
+        // rb.velocity.y = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         if (other.gameObject.CompareTag("Player"))
         {
             IDamageable iDamageable = other.transform.GetComponentInChildren<IDamageable>();
-            iDamageable.TakeDamage(3);
+            iDamageable.TakeDamage(projectileData.damage);
             if (other.gameObject.TryGetComponent(out Knockback knockback))
             {
-                knockback.Apply(gameObject, 10);
+                knockback.Apply(gameObject, projectileData.knockbackForce);
             }
         }
     }
---
+++ Assets/StateMachine/MechaGolem/MechaBossSetupBehaviour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechaBossSetupBehaviour : StateMachineBehaviour
+{
+    private MechaProtect mechaProtect;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        mechaProtect = animator.GetComponent<MechaProtect>();
+        mechaProtect.shield.SetActive(false);
+        mechaProtect.enabled = false;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+}
