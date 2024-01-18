1b6b7ac74f70b5bde1511aea3ee0c29eb9c4e933
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-01-18 15:22:50 +0100

    Clean EnemyAttack

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -5,37 +5,5 @@ using UnityEngine.Events;
 
 public class EnemyAttack : MonoBehaviour
 {
-    [SerializeField]
-    private LayerMask targetLayerMask;
-
-    [SerializeField]
-    private EnemyData enemyData;
-
-    private BoxCollider2D bc2d;
-
-    [SerializeField]
-    private bool isAttacking = false;
-    private Animator animator;
-    private EnemyPatrol enemyPatrol;
-    //  public ContactFilter2D ContactFilter;
-    public UnityEvent OnBegin, OnDone;
-
-    [SerializeField]
-    private float distance = 2f;
-
-    public float attackCooldown = 0;
-    float attackDelay = 5f;
-
-    public bool canMove = true;
-
-    private void Update() {
-        if(attackCooldown > 0) {
-            attackCooldown -= Time.deltaTime;
-        }
-    }
-
-    public bool CanAttack() {
-        return attackCooldown <= 0;
-    }
 
 }
