de8aef8adf0a94d1b82686d7180f26af2f24b345
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-23 11:46:01 +0200

    Change chromatic aberration with car's distance

---
+++ Assets/CameraEffects.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Cinemachine.PostFX;
+using UnityEngine.Rendering.Universal;
+
+public class CameraEffects : MonoBehaviour
+{
+    [SerializeField]
+    private FloatValue distanceTravelled;
+
+    [SerializeField]
+    private float chromaticAberrationStep = 0.025f;
+
+    private float lastThousandth = 0;
+
+    private ChromaticAberration chromaticAberrationClone;
+
+    void Start()
+    {
+        CinemachineVolumeSettings cinemachineVolumeSettings = GetComponent<CinemachineVolumeSettings>();
+
+        if (cinemachineVolumeSettings.m_Profile.TryGet(out ChromaticAberration chromaticAberration))
+        {
+
+            chromaticAberrationClone = chromaticAberration;
+        }
+        chromaticAberrationClone.intensity.value = 0;
+        chromaticAberrationClone.intensity.overrideState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float thousandth = Mathf.Round(distanceTravelled.CurrentValue / 850);
+        if (thousandth >= 1 && thousandth > lastThousandth)
+        {
+            lastThousandth = thousandth;
+            chromaticAberrationClone.intensity.value += chromaticAberrationStep;
+        }
+    }
+}
