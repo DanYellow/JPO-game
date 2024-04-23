b7be0e67e1a86b1282ccbc9fa33ea5663a863f18
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-23 17:19:33 +0200

    Update UI for score

--- Assets/Scripts/CameraEffects.cs
+++ Assets/Scripts/CameraEffects.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using Cinemachine.PostFX;
 using UnityEngine.Rendering.Universal;
 
@@ -11,6 +12,9 @@ public class CameraEffects : MonoBehaviour
     private float chromaticAberrationStep = 0.025f;
 
     private float lastThousandth = 0;
+
+    [SerializeField]
+    private int scoreStepThreshold = 850;
 
     private ChromaticAberration chromaticAberrationClone;
 
@@ -30,12 +34,57 @@ public class CameraEffects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float thousandth = Mathf.Floor(distanceTravelled.CurrentValue / 850);
+        float thousandth = Mathf.Floor(distanceTravelled.CurrentValue / scoreStepThreshold);
         if (thousandth >= 1 && thousandth > lastThousandth)
         {
             print($"Increased ! {thousandth}");
             lastThousandth = thousandth;
             chromaticAberrationClone.intensity.value += chromaticAberrationStep;
+            StartCoroutine(NextThresholdReached(chromaticAberrationClone.intensity.value));
         }
+    }
+
+    private IEnumerator NextThresholdReached(float startValue)
+    {
+        float current = 0;
+        float duration = 0.25f;
+
+        yield return null;
+        while (current <= 1)
+        {
+            chromaticAberrationClone.intensity.value = Mathf.Lerp(startValue, 1,  Spike(current));
+            current += Time.deltaTime / duration;
+
+            yield return null;
+        }
+
+        current = 0;
+        while (current <= 1)
+        {
+            chromaticAberrationClone.intensity.value = Mathf.Lerp(1, startValue, Spike(current));
+
+            current += Time.deltaTime / duration;
+
+            yield return null;
+        }
+
+        chromaticAberrationClone.intensity.value = startValue;
+    }
+
+    private float EaseIn(float t)
+    {
+        return t * t;
+    }
+
+    private float Spike(float t)
+    {
+        if (t <= .5f)
+            return EaseIn(t / .5f);
+        return EaseIn(Flip(t) / .5f);
+    }
+
+    public float Flip(float x)
+    {
+        return 1 - x;
     }
 }
--- TODO.md
+++ TODO.md
@@ -17,6 +17,7 @@
 Appuyez sur <sprite="MV Icons Keyboard - ALL" name="Key_Space_1"><sprite="MV Icons Keyboard - ALL" name="Key_Space_2"><sprite="MV Icons Keyboard - ALL" name="Key_Space_3"> pour interagir
 
 https://github.com/DanYellow/JPO-game/blob/JPO-2024-A/Assets/Scripts/Utils/TimelineController.cs
+
 https://www.google.com/search?q=retro+80s+background+car&sca_esv=2386da0e1e038e73&sca_upv=1&hl=fr&udm=2&biw=2243&bih=1278&ei=DUwlZqKfNrmrkdUPyPSO-Aw&ved=0ahUKEwji2fvd6dOFAxW5VaQEHUi6A88Q4dUDCBA&uact=5&oq=retro+80s+background+car&gs_lp=Egxnd3Mtd2l6LXNlcnAiGHJldHJvIDgwcyBiYWNrZ3JvdW5kIGNhcjIGEAAYCBgeSL4JUKgEWIMIcAF4AJABAJgBbKABuwKqAQMzLjG4AQPIAQD4AQGYAgSgApkCwgIKEAAYgAQYQxiKBcICBhAAGAcYHsICBBAAGB7CAgcQABiABBgTwgIGEAAYExgewgIIEAAYExgFGB7CAggQABgTGAgYHpgDAIgGAZIHAzMuMaAHyAg&sclient=gws-wiz-serp#vhid=Qte3UmCEkIIEgM&vssid=mosaic
 # Ressources
 
