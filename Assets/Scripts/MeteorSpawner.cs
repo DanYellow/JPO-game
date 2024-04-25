44db1c858608a5241c5b0894f13c6af4ade18381
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-25 08:26:06 +0200

    Cleaning code

--- Assets/Scripts/MeteorSpawner.cs
+++ Assets/Scripts/MeteorSpawner.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class MeteorSpawner : MonoBehaviour
 {
-    public GameObject meteorPrefab;
-    public float distance = 20f;
+    [SerializeField]
+    private GameObject meteorPrefab;
+
+    [SerializeField]
+    private float distance = 20f;
 
     private GravityAttractor planet;
 
@@ -13,8 +16,6 @@ public class MeteorSpawner : MonoBehaviour
 
     [SerializeField]
     private Transform target;
-
-    public GameObject prefab;
 
     private void Awake()
     {
@@ -26,40 +27,18 @@ public class MeteorSpawner : MonoBehaviour
         StartCoroutine(SpawnMeteor());
     }
 
-    private void Update()
-    {
-        // Vector3 gravityUp = (target.position - transform.position).normalized;
-        // print(gravityUp);
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //     GameObject go = Instantiate(
-        //          prefab,
-        //          Vector3.zero,
-        //          //  new Vector3(target.position.x, target.position.y, 50 * gravityUp.normalized.y - target.position.z),
-        //          Quaternion.identity
-        //     );
-        //     go.transform.position = new Vector3(
-        //         target.position.x,
-        //         target.position.y,
-        //         target.position.z
-        //     );
-        //     go.transform.LookAt(gameObject.transform);
-        //     // go.transform.rotation = target.rotation;
-        // }
-    }
-
     IEnumerator SpawnMeteor()
     {
         Vector3 pos = new Vector3(
-            target.position.x + (Random.onUnitSphere.x * 20f),
-            target.position.y + (Random.onUnitSphere.y * 20f),
+            target.position.x + (Random.onUnitSphere.x * distance),
+            target.position.y + (Random.onUnitSphere.y * distance),
             target.position.z
         );
         GameObject meteor = Instantiate(meteorPrefab, pos, Quaternion.identity);
         meteor.transform.LookAt(gameObject.transform);
         meteor.GetComponent<GravityBody>().planet = planet;
 
-        yield return Helpers.GetWait(1);
+        yield return Helpers.GetWait(spawnTime);
 
         StartCoroutine(SpawnMeteor());
     }
