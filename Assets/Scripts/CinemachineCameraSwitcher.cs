0e5a3f78a62b2017e18d306284632201a966f47b
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-04-29 20:52:26 +0200

    Hide distance counter on start game

---
+++ Assets/Scripts/CinemachineCameraSwitcher.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CinemachineCameraSwitcher : MonoBehaviour
+{
+    [SerializeField]
+    CinemachineVirtualCamera vcam1;
+
+    [SerializeField]
+    CinemachineVirtualCamera vcam2;
+
+    private bool isVCam1 = true;
+
+    [Header("Scriptable Objects")]
+    [SerializeField]
+    private VoidEventChannel onEvent;
+
+    private void OnEnable()
+    {
+        onEvent.OnEventRaised += SwitchCamera;
+    }
+
+    private void SwitchCamera()
+    {
+        if (isVCam1)
+        {
+            vcam1.Priority = 0;
+            vcam2.Priority = 1;
+        }
+        else
+        {
+            vcam1.Priority = 1;
+            vcam2.Priority = 0;
+        }
+    }
+
+    private void OnDisable()
+    {
+        onEvent.OnEventRaised -= SwitchCamera;
+    }
+}
--- Assets/Scripts/HUDScore.cs
+++ Assets/Scripts/HUDScore.cs
@@ -9,9 +9,33 @@ public class HUDScore : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI scoreText;
 
+    [Header("Scriptable Objects")]
+    [SerializeField]
+    private VoidEventChannel onStartGame;
+
+    private void Awake()
+    {
+        scoreText.gameObject.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        onStartGame.OnEventRaised += DisplayCounter;
+    }
+
+    private void DisplayCounter()
+    {
+        scoreText.gameObject.SetActive(true);
+    }
+
     void Update()
     {
         string _scoreText = $"{Mathf.Round(distanceTravelled.CurrentValue)} m";
         scoreText.SetText(_scoreText);
+    }
+
+    private void OnDisable()
+    {
+        onStartGame.OnEventRaised -= DisplayCounter;
     }
 }
