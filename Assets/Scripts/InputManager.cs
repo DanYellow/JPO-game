131272ff660889e7afd793c37f9dec436ea6207d
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-05-05 00:22:56 +0200

    Rename input events

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 public class ActionMapName
 {
     public static string Drive = "Drive";
-    public static string UI = "UI";
-    public static string GameOverAndCredits = "GameOverAndCredits";
+    public static string Pause = "Pause";
+    public static string MainMenuAndGameOver = "MainMenuAndGameOver";
 }
 
 public class InputManager : MonoBehaviour
@@ -23,19 +23,23 @@ public class InputManager : MonoBehaviour
     [SerializeField]
     private VoidEventChannel onGameOver;
 
+    [SerializeField]
+    private VoidEventChannel onGoBackToMainMenu;
+
     private void Awake()
     {
         onTogglePause.OnEventRaised += ToggleActionMap;
         onGameOver.OnEventRaised += OnGameOver;
+        onGoBackToMainMenu.OnEventRaised += OnGameOver;
 
-        SwitchActionMap(ActionMapName.UI);
+        SwitchActionMap(ActionMapName.Pause);
     }
 
     public void ToggleActionMap(bool isPaused)
     {
         if (isPaused)
         {
-            SwitchActionMap(ActionMapName.UI);
+            SwitchActionMap(ActionMapName.Pause);
         }
         else
         {
@@ -52,7 +56,7 @@ public class InputManager : MonoBehaviour
 
     private void OnGameOver()
     {
-        playerInput.SwitchCurrentActionMap(ActionMapName.GameOverAndCredits);
+        playerInput.SwitchCurrentActionMap(ActionMapName.MainMenuAndGameOver);
     }
 
     public void StartGame()
