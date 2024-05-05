1b4565a047b5eca31a6ae7c40aa4385f686ab560
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-05-05 10:47:18 +0200

    Fix rotation of mainmenu camera

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -7,9 +7,9 @@ public class RotateAround : MonoBehaviour
 
     [SerializeField]
     private float speed = 15;
-    // Update is called once per frame
+
     void Update()
     {
-        transform.RotateAround(pivotPoint.position, new Vector3(0, 1, 0), speed * Time.deltaTime);
+        transform.RotateAround(pivotPoint.position, pivotPoint.up, speed * Time.deltaTime);
     }
 }
