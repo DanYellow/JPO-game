61c589172eba9838a2e2428a8007bb76d5118bad
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-05-12 15:44:20 +0200

    Add editor script to generate build in a folder with current time as name

--- Assets/Editor/BuildPlayerWithVersion.cs
+++ Assets/Editor/BuildPlayerWithVersion.cs
@@ -2,40 +2,36 @@ using UnityEditor;
 using UnityEngine;
 using UnityEditor.Build.Reporting;
 using System.IO;
+using System.Text.RegularExpressions;
 
 // https://stackoverflow.com/questions/75630802/create-new-folder-for-the-project-to-get-build-into-throught-script-c-sharp
 public class BuildPlayerWithVersion : MonoBehaviour
 {
-    [MenuItem("Build/Build With Version")]
+    [MenuItem("**Debug**/Build/Create build with current time")]
     public static void MyBuild()
     {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions(); 
-        buildPlayerOptions.target = BuildTarget.StandaloneWindows;
-        // BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(buildPlayerOptions);
-
-        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "Builds", "");
-        // Get your build version here
+        BuildPlayerOptions buildPlayerOptions = BuildPlayerWindow.DefaultBuildMethods.GetBuildPlayerOptions(new BuildPlayerOptions());
         string version = System.DateTime.Now.ToString("y-MM-dd-'h'-HH-mm-ss");
-        // string path = Path.Join("Builds", "");
-        // print(buildPlayerOptions.locationPathName);
-        print(version);
 
-        buildPlayerOptions.locationPathName = path;
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        string productNameWithExt = Regex.Match(buildPlayerOptions.locationPathName, @"" + Application.productName + ".([A-z])\\w+").Groups[0].ToString();
+        string path = buildPlayerOptions.locationPathName.Replace(productNameWithExt, "");
 
-        // Directory.CreateDirectory(path);      
-        
-        // BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        // BuildSummary summary = report.summary;
+        string finalPath = Path.Join(path, version);
+        Directory.CreateDirectory(finalPath);
 
-        // if (summary.result == BuildResult.Succeeded)
-        // {
-        //     Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
-        // }
+        buildPlayerOptions.locationPathName = $"{finalPath}/{productNameWithExt}";
 
-        // if (summary.result == BuildResult.Failed)
-        // {
-        //     Debug.Log("Build failed");
-        // }
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
     }
 }
