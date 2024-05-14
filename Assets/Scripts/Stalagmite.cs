806ad2f53ffd2e59965139c0b538f4bfdb3dafd8
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-05-14 16:37:36 +0200

    Animate Stalagmite position

--- Assets/Scripts/Stalagmite.cs
+++ Assets/Scripts/Stalagmite.cs
@@ -7,6 +7,8 @@ public class Stalagmite : MonoBehaviour
     [SerializeField]
     private Transform target;
 
+    private Vector3 endPosition;
+
     [Header("Scriptable Objects")]
     [SerializeField]
     private VoidEventChannel onGameOver;
@@ -18,9 +20,30 @@ public class Stalagmite : MonoBehaviour
         SphereCollider sphereCollider = target.GetComponent<SphereCollider>();
         float sphereColliderScale = sphereCollider.transform.lossyScale.x;
         BoxCollider boxCollider = GetComponent<BoxCollider>();
-        transform.position = Random.onUnitSphere * (sphereCollider.radius + (boxCollider.bounds.size.y * 2 / sphereColliderScale)) * sphereColliderScale;
+        transform.position = Vector3.zero;
+        endPosition = Random.onUnitSphere * (sphereCollider.radius + (boxCollider.bounds.size.y * 2 / sphereColliderScale)) * sphereColliderScale;
         transform.up = target.up;
-        transform.rotation = Quaternion.LookRotation(target.position - transform.position);
+
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        float current = 0;
+        float duration = 0.65f;
+        Vector3 startPosition = transform.position;
+
+        while (current <= 1)
+        {
+            transform.position = Vector3.Lerp(startPosition, endPosition, current);
+            current += Time.deltaTime / duration;
+            if (transform.position != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(target.position - transform.position);
+            }
+
+            yield return null;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
