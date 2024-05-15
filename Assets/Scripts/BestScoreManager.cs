ca713536b4a0bf4dbb35cd99696839d219faec18
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-05-15 13:28:33 +0200

    wip - display best score

---
+++ Assets/Scripts/BestScoreManager.cs
@@ -0,0 +1,65 @@
+using System.Globalization;
+using UnityEngine;
+using TMPro;
+
+public class BestScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bestScoreUI;
+
+    [Header("Scriptable Objects")]
+    [SerializeField]
+    private VoidEventChannel onGameOver;
+
+    [SerializeField]
+    private FloatValue distanceTravelled;
+
+    private void OnEnable()
+    {
+        onGameOver.OnEventRaised += SaveRecord;
+    }
+
+    private void UpdateLayout(bool hasBrokenPrevRecord)
+    {
+        var nfi = (NumberFormatInfo)CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = " ";
+
+        string currentRecordFormatted = Mathf.Round(PlayerPrefs.GetFloat("best_score")).ToString("#,0", nfi);
+
+        TextMeshProUGUI recordText = bestScoreUI.GetComponent<TextMeshProUGUI>();
+        if (hasBrokenPrevRecord)
+        {
+            PlayerPrefs.SetFloat("best_score", distanceTravelled.CurrentValue);
+            
+            string newRecordFormatted = Mathf.Round(distanceTravelled.CurrentValue).ToString("#,0", nfi);
+            recordText.SetText(@$"
+                Nouveau record !
+                {currentRecordFormatted} m -> {newRecordFormatted} m
+            ");
+        }
+        else
+        {
+            recordText.SetText(@$"Record actuel : {currentRecordFormatted} m");
+        }
+    }
+
+    private void SaveRecord()
+    {
+        bool hasBrokenPrevRecord = (
+            !PlayerPrefs.HasKey("best_score") ||
+            PlayerPrefs.HasKey("best_score") && distanceTravelled.CurrentValue > PlayerPrefs.GetFloat("best_score")
+        );
+
+        if (hasBrokenPrevRecord)
+        {
+            // PlayerPrefs.SetFloat("best_score", distanceTravelled.CurrentValue);
+        }
+
+        UpdateLayout(hasBrokenPrevRecord);
+    }
+
+    private void OnDisable()
+    {
+        onGameOver.OnEventRaised -= SaveRecord;
+    }
+}
