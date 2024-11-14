8017203a3405cfde6fd21f4c3ccf359a38109eb9
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-14 16:20:46 +0100

    Add InputAction for Player1

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,113 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField, Range(1, 15)]
+    private float jumpForce = 10;
+
+    private bool isGrounded = false;
+
+    private bool isFalling = false;
+
+    private bool isGroundPounding = false;
+
+    [SerializeField, Range(1, 15)]
+    private float groundPoundCooldown = 2.75f;
+    private float lastGroundPoundCooldown = 0;
+
+    [SerializeField]
+    private LayerMask listGroundLayers;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private float groundCheckRadius;
+
+    [SerializeField, Range(1, 15)]
+    private float dropForce = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        isFalling = IsFalling();
+        isGrounded = IsGrounded();
+
+        if (!isGrounded && !isGroundPounding)
+        {
+            Vector3 vel = rb.velocity;
+            vel.y -= 10.5f * Time.deltaTime;
+            rb.velocity = vel;
+        }
+    }
+
+    public bool IsFalling()
+    {
+        return rb.velocity.y < 0;
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics.OverlapSphere(groundCheck.position, groundCheckRadius, listGroundLayers).Length > 0;
+    }
+
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (Time.time - lastGroundPoundCooldown > groundPoundCooldown)
+        {
+            return;
+        }
+
+        if (!isGrounded)
+        {
+            StopAndSpin();
+        }
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+
+    private void StopAndSpin()
+    {
+        isGroundPounding = true;
+        lastGroundPoundCooldown = Time.time;
+
+        StartCoroutine(DropAndPound());
+    }
+
+    private IEnumerator DropAndPound()
+    {
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+        yield return new WaitForSeconds(0.15f);
+        rb.constraints = RigidbodyConstraints.None;
+        rb.AddForce(Vector3.down * dropForce, ForceMode.Impulse);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
+    }
+}
