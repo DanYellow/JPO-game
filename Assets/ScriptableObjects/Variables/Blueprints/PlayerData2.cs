f19dc742e4198ec0a2c908e6ad5e822235c95e13
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-15 17:07:03 +0100

    WIP - Disable field inherited through property

---
+++ Assets/ScriptableObjects/Variables/Blueprints/PlayerData2.cs
@@ -0,0 +1,9 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Player Data2", menuName = "ScriptableObjects/Values/PlayerData2", order = 0)]
+public class PlayerData2 : PlayerData
+{
+
+
+}
