46e43a7aab7418c97e80f06e018636e0f5870c1c
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-16 13:58:06 +0100

    Display player's name from script

--- Assets/Scripts/PlayerHealthIndicator.cs
+++ Assets/Scripts/PlayerHealthIndicator.cs
@@ -1,19 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
 
 public class PlayerHealthIndicator : MonoBehaviour
 {
+    private Dictionary<PlayerID, string> playerNameDict = new Dictionary<PlayerID, string>(){
+        {PlayerID.Player1, "Joueur 1"},
+        {PlayerID.Player2, "Joueur 2"},
+        {PlayerID.Player3, "Joueur 3"},
+        {PlayerID.Player4, "Joueur 4"}
+    };
+
+    [SerializeField]
+    private Image playerImage;
+    [SerializeField]
+    private TextMeshProUGUI playerName;
+
     [Header("Scriptable Objects"), SerializeField]
     private PlayerData playerData;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        playerName.SetText($"{playerNameDict[playerData.id]}");
     }
 }
