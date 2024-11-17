dafc55ff6239880bee7527bdcbc009ed8212014b
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-17 16:25:50 +0100

    Rename PlayerPosition class

--- Assets/Scripts/PlayerPosition.cs
+++ Assets/Scripts/PlayerPosition.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
-public class Playerposition : MonoBehaviour
+public class PlayerPosition : MonoBehaviour
 {
     private float heightSprite;
 
-    private void Awake() {
+    private void Awake()
+    {
         heightSprite = GetComponentInChildren<SpriteRenderer>().bounds.size.y / 2;
     }
 
-    public void SetPosition(Transform transform) {
+    public void SetPosition(Transform transform)
+    {
         GetComponent<Rigidbody>().position = new Vector3(
             transform.position.x,
             transform.position.y + heightSprite,
