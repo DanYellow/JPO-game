f3419978f566073ba6b0ec70164793d610b79e8c
PC-Dan <jeanlouis.danielo@sfr.fr>
2024-11-17 23:12:36 +0100

    Add script to customize button

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,73 @@
+using UnityEngine;  
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class UIButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, ISelectHandler, IDeselectHandler
+{
+    private TextMeshProUGUI textContainer;
+
+    [SerializeField]
+    private Color hoverColor = Color.red;
+
+    [SerializeField]
+    private Color pressedColor = Color.red;
+    [SerializeField]
+    private Color disabledColor = Color.gray;
+
+    private string originalText;
+
+    private Color originalColor;
+
+    [SerializeField]
+    private bool keepImageButton = false;
+
+    private Button button;
+
+    private void Awake() {
+        textContainer = GetComponentInChildren<TextMeshProUGUI>();
+        button = GetComponent<Button>();
+        button.GetComponent<Image>().enabled = keepImageButton;
+
+        originalColor = textContainer.color;
+        originalText = textContainer.text;
+    }
+
+    private void Update() {
+        if(button.interactable == false) {
+            textContainer.color = disabledColor;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        textContainer.color = hoverColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        textContainer.color = originalColor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        textContainer.color = pressedColor;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        if(eventData.selectedObject == gameObject) {
+            textContainer.color = hoverColor;
+            // textContainer.SetText($"►{originalText}◄");
+        }
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        if(eventData.selectedObject == gameObject) {
+            textContainer.color = originalColor;
+            // textContainer.SetText(originalText);
+        }
+    }
+}
--- TODO.md
+++ TODO.md
@@ -1,10 +1,12 @@
 - ~~Display UI for player's lives~~
 - ~~Display game end canvas~~
 - Make main menu
-- Modelize player's base
+- ~~Modelize player's base~~
 
 Credits : https://opengameart.org/content/smoke-aura
 
 https://www.youtube.com/watch?v=50qv78W5vko
 
 effect : https://www.youtube.com/watch?v=y36r_Pd8bjk
+
+FFC000
